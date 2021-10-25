ca7494dd15255256a458d88fb9156b9dfa618686
vLucky <65517973+vLuckyyy@users.noreply.github.com>
2021-10-25 23:34:02 +0200

    Create HelloWorld.vb

---
+++ Visual Basic/HelloWorld.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module HelloWorld
+    Sub Main()
+
+        Console.WriteLine("Hello World!")
+
+        Console.WriteLine("Press Enter Key to Exit.")
+
+        Console.ReadLine()
+
+    End Sub 
+
+End Module
